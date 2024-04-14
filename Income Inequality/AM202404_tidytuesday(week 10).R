9957a013b08fe46f8deef96ccf383eeda481da32
Azael J. Mateo Mendoza <57370907+AzaelMateo@users.noreply.github.com>
2024-04-13 19:36:07 -0600

    Update and rename AM202103_tidytuesday(week 10).R to AM202404_tidytuesday(week 10).R

--- Income Inequality/AM202404_tidytuesday(week 10).R
+++ Income Inequality/AM202404_tidytuesday(week 10).R
@@ -1,7 +1,7 @@
 
 rm(list = ls())
-cls <- function() cat(rep("\n",100))
-wd <- "E:/Azael Personal/Documentos/R/tidytuesday"
+cls <- function() cat(rep("/n",100))
+wd <- "D:/Users/MAMA95C3/Documents/R/Otros"
 setwd(wd)
 cls()
 
@@ -14,7 +14,7 @@ cls()
 # Purpose: This file generates the script to build a vis of ...
 #
 # Created files: -
-#**************************************************************************************************************************************************/
+#/************************************************************************************************************************************************/
 
 
 library(tidyverse)
@@ -71,13 +71,13 @@ ggplot() +
   geom_line(data = line_data, aes(x = per, y = value, color = name), size = 2) +
   geom_point(data = dots, aes(x = per, y = value, color = name)) +
   geom_text(data = labels, aes(x = per, y = value, label = scales::dollar(diff)), family = "Goldman Sans Condensed Regular", nudge_x = c(-0.7, 0.7), size = 7) +
-  annotate(GeomRichtext, x = 2009.5, y = 5100, label = glue("**Cerca, Pero No Lo Suficiente**: Brecha salarial entre<br>{highlight_text('Mujeres', '#e01a4f', 'b', size = 40)} y {highlight_text('Hombres', '#53b3cb', 'b', size = 40)} en México de 2005 a 2020"), 
+  annotate(GeomRichtext, x = 2009.5, y = 8500, label = glue("**Cerca, Pero No Lo Suficiente**: Brecha salarial entre<br>{highlight_text('Mujeres', '#e01a4f', 'b', size = 40)} y {highlight_text('Hombres', '#53b3cb', 'b', size = 40)} en MÃ©xico de 2005 a 2023"), 
            hjust = 0, vjust = 1, size = 10, family = "Goldman Sans Condensed", label.color = NA) +
   labs(x = NULL, 
        y = NULL,
-       caption = "Fuente: Coneval 2021. Precios Reales. | Elaborado por Azael Mateo (@xzxxlmxtxx)") +
-  scale_x_continuous(limits = c(2004, 2021), breaks = seq(2005, 2020, 1)) +
-  scale_y_continuous(limits = c(3300, 5250), breaks = seq(3500, 5000, 300), labels = c("$3,500", "$3,800", "$4,100", "$4,400", "$4,700", "$5,000")) +
+       caption = "Fuente: INEGI 2023. Precios Reales 2020. | Elaborado por Azael Mateo (@xzxxlmxtxx)") +
+  scale_x_continuous(limits = c(2004, 2024), breaks = seq(2005, 2023, 1)) +
+  scale_y_continuous(limits = c(4300, 9250), breaks = seq(4500, 9000, 500), labels = c("$4,500", "$5,000", "$5,500", "$6,000", "$6,500", "$7,000", "$7,500", "$8,000", "$8,500", "$9,000")) +
   scale_color_manual(values = pal) +
   theme_tufte()+
   theme(
@@ -90,4 +90,4 @@ ggplot() +
 
 
 # Export --------------------------------------------------------------------------------------------------------------------------------------------
-ggsave("AM202103_BrechSal (week 10).png", width = 18, height = 9)
+ggsave("AM202404_BrechSal (week 10).png", width = 17, height = 9)
